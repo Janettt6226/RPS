10815f5bbe13db8367044cfde76cd232dc2b386e
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-11-01 11:08:12 +0100

    5 points game implemented

--- main.js
+++ main.js
@@ -2,15 +2,91 @@ console.log("coucou");
 
 const buttons = document.querySelectorAll("button");
 let playerSelection;
-// const rock = document.querySelector('#rock');
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
-// rock.addEventListener("click", function() {
-//   console.log(rock.value);
-// });
-buttons.forEach(button =>{
-  button.addEventListener('click', function(){
-    console.log(this.value);
+
+// function getPlayerChoice() {
+//   return playerSelection;
+// }
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  choices.sort(() => Math.random() -0.5);
+  computerSelection = choices[0];
+  return computerSelection;
+}
+
+function playRound(playerSelection) {
+  // playerSelection = getPlayerChoice();
+  computerSelection = getComputerChoice();
+  let result;
+  let scoreCount;
+  if ((playerSelection == "rock" && computerSelection == "scissors")
+  || (playerSelection == "scissors" && computerSelection == "paper")
+  || (playerSelection == "paper" && computerSelection == "rock"))  {
+      result = "You won!" + " " + playerSelection + " " + "beats" + " " + computerSelection + "!";
+      playerScore++
+      scoreCounter();
+      if (playerScore == 5) {
+        result = "You won. Do you want to play again?"
+        disableButtons();
+      }
+    } else if (playerSelection == computerSelection){
+      result = "Tie game!"
+    } else {
+      computerScore++
+      result = "You lose, " + " " + computerSelection + " " + "beats" + " " + playerSelection + "!";
+      scoreCounter();
+    } if (computerScore == 5) {
+      result = "You lose. Do you want to play again?"
+      disableButtons();
+    }
+    console.log(result);
+    roundResult.innerText = result;
+    roundSection.appendChild(roundResult);
+  }
+
+
+  const roundContainer = document.querySelector(".round-container");
+  const roundSection = document.createElement("div");
+  const roundResult = document.createElement("h2");
+  const counter = document.createElement("p");
+  roundContainer.appendChild(roundSection);
+
+  function scoreCounter() {
+    scoreCount = "computer:" + " " + computerScore + " " + "pts" + " vs " + "player:" + " " + playerScore + " " + "pts"
+    counter.innerText = scoreCount;
+    roundSection.appendChild(counter);
+
+  }
+
+  // function game() {
+  //   for (let i = 0; i < 100; i++) {
+  //     let message = "";
+  //     if (playerScore === 5) {
+  //       message = "Congratulation player, you won this game!";
+  //       break;
+  //     } else if (computerScore === 5) {
+  //       message = "Too bad, computer beat you!";
+  //       break;
+  //     } else if (computerScore < 5 || playerScore < 5) {
+  //       playRound();
+  //     }
+  //   }
+  // }
+
+
+  buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+      // playerSelection = this.value;
+      playRound(this.value);
+    })
   })
-  playerSelection = this.value;
-  return playerSelection;
-})
+
+  function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
